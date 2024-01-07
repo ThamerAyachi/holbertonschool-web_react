a6fb0f91d6d782194f3a38014a38253f2da61d9c
ThamerAyachi <thamerjak3@gmail.com>
2024-01-07 23:14:58 +0100

    fix pack

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,22 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	entry: './js/dashboard_main.js',
+	entry: "./js/dashboard_main.js",
 	output: {
-		path: path.resolve(__dirname, 'public'),
-		filename: 'bundle.js',
+		path: path.resolve(__dirname, "public"),
+		filename: "bundle.js",
 	},
 	module: {
 		rules: [
 			{
 				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
+				use: [
+					"style-loader",
+					"css-loader",
+					"file-loader",
+					"image-webpack-loader",
+				],
 			},
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/resource',
+				type: "asset/resource",
 			},
 		],
 	},
-	mode: 'production',
+	mode: "production",
 };
+
